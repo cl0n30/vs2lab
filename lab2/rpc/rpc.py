c0897b38dbf94329d13ff3c9b36e4e2cb6d670c8
Oliver Reiser <reiser646@gmail.com>
2024-05-01 17:04:30 +0200

    solution lab2

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,4 +1,6 @@
 import constRPC
+import threading
+import time
 
 from context import lab_channel
 
@@ -25,13 +27,49 @@ class Client:
     def stop(self):
         self.chan.leave('client')
 
-    def append(self, data, db_list):
-        assert isinstance(db_list, DBList)
-        msglst = (constRPC.APPEND, data, db_list)  # message payload
-        self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
-
+    def append(self, data, db_list, callback = None):
+        background = AsyncAppend(data, db_list, self.chan, callback, self.error)
+        background.start()
+        self.working()
+        background.join()
+        print("Client waited until background work was done")
+        
+    def working(self):
+        print("Client working")
+        print("Client doing stuff in the foreground")
+        print("Client doing more stuff")
+        print("Client doing some more stuff")
+        print("Client doing more things")
+        
+    def error(self, msg):
+        print("Error: "+msg)
+ 
+class AsyncAppend(threading.Thread):
+    def __init__(self, data, db_list, chan:lab_channel.Channel, callback = None, error = None):
+        threading.Thread.__init__(self)
+        self.data = data
+        self.db_list = db_list
+        self.chan = chan
+        self.server = self.chan.subgroup('server')
+        self.timeout = 3
+        self.callback = callback
+        self.error = error
+        
+    def run(self):
+        self.startAppend()
+        
+    def startAppend(self):
+        assert isinstance(self.db_list, DBList)
+        msglst = (constRPC.APPEND, self.data, self.db_list)  # message payload
+        self.chan.send_to(self.server, msglst)
+        msgack = self.chan.receive_from(self.server, self.timeout) # wait for ACK
+        if msgack == None:
+            if self.error:
+                self.error("No ACK received")
+            return
+        msgrcv = self.chan.receive_from(self.server) # wait for response
+        if self.callback:
+            self.callback(msgrcv[1])
 
 class Server:
     def __init__(self):
@@ -52,7 +90,20 @@ class Server:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
                 if constRPC.APPEND == msgrpc[0]:  # check what is being requested
-                    result = self.append(msgrpc[1], msgrpc[2])  # do local call
-                    self.chan.send_to({client}, result)  # return response
+                    #time.sleep(5) #too long to send ack
+                    connEst = self.sendAck(client)
+                    if connEst:
+                        time.sleep(10)
+                        result = self.append(msgrpc[1], msgrpc[2])  # do local call
+                        self.chan.send_to({client}, result)  # return response
+                    else:
+                        print("Client unavailable, continuing...")
                 else:
                     pass  # unsupported request, simply ignore
+                
+    def sendAck(self, client):
+        if self.chan.exists(client):
+            self.chan.send_to({client}, (constRPC.ACK, "append"))
+            return True
+        else:
+            return False
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -8,9 +8,10 @@ lab_logging.setup(stream_level=logging.INFO)
 cl = rpc.Client()
 cl.run()
 
-base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+def printResult(result):
+  print("Result: {}".format(result.value))
 
-print("Result: {}".format(result_list.value))
+base_list = rpc.DBList({'foo'})
+cl.append('bar', base_list, printResult)
 
 cl.stop()
